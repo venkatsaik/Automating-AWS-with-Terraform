e9e32cfdea9d176ab4b174b72a81095941908655
venkat sai kusumurthy <venkatsaikusumurthy@venkats-mbp.mynetworksettings.com>
2023-02-19 10:38:16 -0500

    added private ec2 instance and sceurity group

--- instance.tf
+++ instance.tf
@@ -1,3 +1,5 @@
+
+#creating public ec2-instance
 resource "aws_instance" "public" {
   ami                         = "ami-002070d43b0a4f171"
   associate_public_ip_address = true
@@ -12,6 +14,22 @@ resource "aws_instance" "public" {
   }
 }
 
+#creating private ec2-instance
+resource "aws_instance" "private" {
+  ami                    = "ami-002070d43b0a4f171"
+  instance_type          = "t3.micro"
+  key_name               = "keypair"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[0].id
+
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+
+#security for public instance
 resource "aws_security_group" "public" {
   name        = "${var.env_code}-public"
   description = "Allow inbound traffic"
@@ -26,14 +44,42 @@ resource "aws_security_group" "public" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "${var.env_code}-public"
   }
 }
+
+
+#security for private instance
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "allow port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["100.1.162.109/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+
