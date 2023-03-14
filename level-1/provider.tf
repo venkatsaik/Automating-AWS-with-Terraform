0e8140114ef1afcd101b563c2b20a912c05ac113
venkat sai kusumurthy <venkatsaikusumurthy@venkats-mbp.mynetworksettings.com>
2023-03-14 11:22:45 -0400

    level-1

---
+++ level-1/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-automation-remotestate"
+    key            = "level-1.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
