d19193541ac5e6e4477c1094b4ac4650676c06f5
venkatsaik <venkatsaik101@gmail.com>
2023-03-28 13:01:02 -0400

    error

--- level-2/instance.tf
+++ level-2/instance.tf
@@ -98,6 +98,14 @@ resource "aws_security_group" "private" {
     cidr_blocks = [data.terraform_remote_state.level-1.outputs.vpc_cidr]
   }
 
+  ingress {
+    description = "HTTP from load_balancer"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
